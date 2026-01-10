9624c2218e86ad0cc4b40b9098b393a94f2095f3
Matthias LEROUX <matthias.leroux@ml2i.fr>
2026-01-10 19:17:35 +0100

    feat(jwt): implement token generation with jjwt library

--- backend/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ backend/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,30 @@
 package com.openclassrooms.etudiant.service;
 
-
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class JwtService {
+    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long jwtExpirationMs = 86400000; // 24 heures
 
     public String generateToken(UserDetails userDetails) {
-        return null; // TODO
-    }
+        Map<String, Object> claims = new HashMap<>();
 
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(secretKey)
+                .compact();
+    }
 }
